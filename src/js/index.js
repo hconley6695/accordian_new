22ab7fb6f522aefdd74ab70f03cb71f4a3275caf
Heather Conley <hconley6695@gmail.com>
2016-03-09 00:19:45 -0500

    tried to get boxes to move in turn with eventListener and removeClass

--- src/js/index.js
+++ src/js/index.js
@@ -20,21 +20,37 @@ $('#header3').on('click', function () {
 	//$('.box3').addClass('box3');
 	$('.box1').addClass('moveup');
 	$('.box2').addClass('moveup');
-
+	//$('.box1').addClass('moveup').removeClass('moveup');
+	//$('.box2').addClass('moveup').removeClass('moveup');
+	//$('.box1').removeClass('moveup');
+	//$('.box2').removeClass('moveup');
 });
 
 
 $('#header2').on('click', function () {
 	$('.box1').addClass('moveup');
 	$('.box3').addClass('moveup');
-
+	//$('.box1').addClass('moveup').removeClass('moveup');
+	//$('.box3').addClass('moveup').removeClass('moveup');
+	//$('.box1').removeClass('moveup');
+	//$('.box3').removeClass('moveup');
 });
 
 $('#header1').on('click', function () {
 	$('.box2').addClass('moveup');
 	$('.box3').addClass('moveup');
-
+	//$('.box2').addClass('moveup').removeClass('moveup');
+	//$('.box3').addClass('moveup').removeClass('moveup');
+	//$('.box2').removeClass('moveup');
+	//$('.box3').removeClass('moveup');
 });
+
+//var boxes = document.query.Selector('.app');
+
+//boxes.addEventListener('click', function(move) {
+//	var upanddown = move.target.textContent;
+	//return upanddown;
+//});
 
 	
 
